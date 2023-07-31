af58a4422860cd99f6711b2f18a4c9426b591208
kimkundad <kim.kundad@gmail.com>
2023-08-01 03:24:48 +0700

    kim

--- resources/views/admin/donate/index.blade.php
+++ resources/views/admin/donate/index.blade.php
@@ -68,7 +68,7 @@
                     <div class="card card-xl-stretch mb-5 mb-xl-8">
                         <div class="card-header border-0 pt-5">
                             <h3 class="card-title align-items-start flex-column">
-                                <span class="card-label fw-bold fs-3 mb-1">ยอดบริจาคทั้งหมด -  {{ number_format($sum,2) }} บาท</span>
+                                <span class="card-label fw-bold fs-3 mb-1">ยอดบริจาคทั้งหมด + {{ number_format($sum,2) }} บาท</span>
                                 <span class="text-muted mt-1 fw-semibold fs-7"> จำนวนยอดบริจาค {{ $count }} ยอด</span>
                             </h3>
                         </div>
